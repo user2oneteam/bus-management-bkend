a096cd916a0ac8da6aa323bacf1825261104f0c8
user2oneteam <user2oneteam@gmail.com>
2025-06-05 22:31:45 -0400

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -88,6 +88,8 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
+define( 'WP_HOME', 'http://bkend-bms-fna6bwdsavczgchc.canadacentral-01.azurewebsites.net' );
+define( 'WP_SITEURL', 'http://bkend-bms-fna6bwdsavczgchc.canadacentral-01.azurewebsites.net' );
 
 
 
